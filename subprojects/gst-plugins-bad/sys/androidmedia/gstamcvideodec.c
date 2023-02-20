fcd2908f1de5b130285ec4cda69e8b214402f1cd
Alexander Nikishin <nikishin89@gmail.com>
2023-02-20 19:30:19 +0300

    Removed disabled code; added logs

--- subprojects/gst-plugins-bad/sys/androidmedia/gstamcvideodec.c
+++ subprojects/gst-plugins-bad/sys/androidmedia/gstamcvideodec.c
@@ -2444,7 +2444,7 @@ gst_amc_video_dec_decide_allocation (GstVideoDecoder * bdec, GstQuery * query)
   GstAmcVideoDec *self = GST_AMC_VIDEO_DEC (bdec);
   gboolean need_pool = FALSE;
   GstCaps *caps = NULL;
-//  GError *error = NULL;
+  GError *error = NULL;
 
   if (!GST_VIDEO_DECODER_CLASS (parent_class)->decide_allocation (bdec, query))
     return FALSE;
@@ -2459,7 +2459,6 @@ gst_amc_video_dec_decide_allocation (GstVideoDecoder * bdec, GstQuery * query)
 
     if (!_find_local_gl_context (self))
       goto out;
-#if 0
     if (!self->gl_context) {
       GST_OBJECT_LOCK (self->gl_display);
       do {
@@ -2467,10 +2466,12 @@ gst_amc_video_dec_decide_allocation (GstVideoDecoder * bdec, GstQuery * query)
           gst_object_unref (self->gl_context);
           self->gl_context = NULL;
         }
+        GST_DEBUG_OBJECT (self, "Just get a GL context. We don't care");
         /* just get a GL context.  we don't care */
         self->gl_context =
             gst_gl_display_get_gl_context_for_thread (self->gl_display, NULL);
         if (!self->gl_context) {
+          GST_DEBUG_OBJECT (self, "Current context is NULL");
           if (!gst_gl_display_create_context (self->gl_display,
                   self->other_gl_context, &self->gl_context, &error)) {
             GST_OBJECT_UNLOCK (mix->display);
@@ -2481,7 +2482,6 @@ gst_amc_video_dec_decide_allocation (GstVideoDecoder * bdec, GstQuery * query)
               self->gl_context));
       GST_OBJECT_UNLOCK (self->gl_display);
     }
-#endif
 
     self->downstream_supports_gl = TRUE;
   }
